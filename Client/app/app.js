5c0ecee4f7ad628f9744b94c31470bf7edb3f7e7
Scott Gago <scottgago@gmail.com>
2016-05-13 08:53:23 -0700

    added templates

--- Client/app/app.js
+++ Client/app/app.js
@@ -1,7 +1,22 @@
 angular.module("heatingUp",['ngRoute'])
 .config(function($routeProvider){
   $routeProvider
-    .when('/login',{
+    .when('/',{
       templateUrl: 'app/login/login.html'
+    })
+    .when('#',{
+      templateUrl: 'app/login/login.html'
+    })
+    .when('/byPlayer',{
+      templateUrl: 'app/byPlayer/byPlayer.html'
+    })
+    .when('/byTeam',{
+    	templateUrl: 'app/byTeam/byTeam.html'
+    })
+    .when('/byConference',{
+    	templateUrl: 'app/byConference/byConference.html'
+    }).
+    otherwise({
+    	templateUrl: 'app/login/login.html'
     })
 })
